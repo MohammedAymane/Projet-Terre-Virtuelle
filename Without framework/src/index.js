8a958863a335bc03c7fbd1b3fb2aaf99932abdfa
Aymane <mohammed-aymane.ouggadi@imt-atlantique.net>
2021-10-16 18:29:31 +0200

    new branche

--- Without framework/src/index.js
+++ Without framework/src/index.js
@@ -1,5 +1,8 @@
-var Cesium = require('cesium/Cesium');
-require('./css/main.css');
-require('cesium/Widgets/widgets.css');
+var Cesium = require("cesium/Cesium");
+require("./css/main.css");
+require("cesium/Widgets/widgets.css");
 
-var viewer = new Cesium.Viewer('cesiumContainer');
+var viewer = new Cesium.Viewer("cesiumContainer");
+viewer.dataSources.add(
+  Cesium.CzmlDataSource.load("https://testyyy.free.beeceptor.com/")
+);
